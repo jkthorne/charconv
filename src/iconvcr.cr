8bd556292189b648e593e34c5d2815e2fd6dbde2
Jack Thorne <jack@kilmer.co>
2026-03-10 10:16:16 -0500

    Wire up public API with convert, encoding_supported?, list_encodings

--- src/iconvcr.cr
+++ src/iconvcr.cr
@@ -1,6 +1,29 @@
-# TODO: Write documentation for `Iconvcr`
+require "./iconvcr/types"
+require "./iconvcr/decode"
+require "./iconvcr/encode"
+require "./iconvcr/registry"
+require "./iconvcr/converter"
+
 module Iconvcr
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class ConversionError < Exception
+  end
+
+  def self.convert(input : Bytes, from : String, to : String) : Bytes
+    converter = Converter.new(from, to)
+    converter.convert(input)
+  end
+
+  def self.convert(input : String, from : String, to : String) : Bytes
+    convert(input.to_slice, from, to)
+  end
+
+  def self.encoding_supported?(name : String) : Bool
+    !Registry.lookup(name).nil?
+  end
+
+  def self.list_encodings : Array(String)
+    Registry.canonical_names
+  end
 end
